6ae81594b2c818c40b1cb2341e5e1e98d3e4d2f3
minpppal <minpal203@gmail.com>
2024-07-19 21:37:47 +0900

    Signin 페이지 css, 글로벌 Font 추가

---
+++ public/svg/GoogleIcon.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const googleIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="19"
+    height="19"
+    viewBox="0 0 19 19"
+    fill="none"
+    style={{ marginRight: '10px' }}>
+    <path
+      d="M14.8212 3.95491C13.461 2.74042 11.6494 2 9.66335 2C5.43044 2 2 5.35849 2 9.5C2 13.6415 5.43044 17 9.66335 17C14.8671 17 16.79 13.1444 17 10.1251H9.5"
+      stroke="#4D4D4D"
+      stroke-width="3"
+    />
+  </svg>
+)
+
+export default googleIcon
--- src/app/login/signin/page.tsx
+++ src/app/login/signin/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 import React from 'react'
 import styles from '@/app/login/signin/page.module.scss'
+import GoogleIcon from '../../../../public/svg/GoogleIcon'
+import Stroke from '../../../../public/svg/Stroke'
+import LoginPageArrow from '../../../../public/svg/LoginPageArrow'
 const Page = () => {
   return (
-    <>
+    <div className={styles.parentContainer}>
+      <LoginPageArrow />
+      <div className={styles.backgroundContainer}></div>
       <div className={styles.mainContainer}>
         <div className={styles.loginTitleBox}>
           <div className={styles.loginTitle}>Sign in</div>
@@ -12,22 +17,46 @@ const Page = () => {
           </div>
         </div>
         <div className={styles.inputContainer}>
-          <div className={styles.googleSignInButton}></div>
-          <div className={styles.underline}></div>
-          <div className={styles.loginInput}></div>
-          <div className={styles.passwordInput}></div>
-          <div className={styles.checkContainer}>
-            <div className={styles.checkBox}></div>
-            <div className={styles.forgotPassword}></div>
+          <div className={styles.googleSignInButton}>
+            <GoogleIcon />
+            Sign in with Google
           </div>
-          <div className={styles.signIn}></div>
+          <Stroke />
+          <div className={styles.loginInputId}>
+            <input
+              className={styles.idInput}
+              placeholder="아이디를 입력해주세요."
+            />
+          </div>
+          <div className={styles.loginInputPassword}>
+            <input
+              className={styles.passwordInput}
+              type="password"
+              placeholder="비밀번호를 입력해주세요."
+            />
+          </div>
+          <div className={styles.checkContainer}>
+            <input
+              className={styles.checkBox}
+              type="checkbox"
+            />
+            <label className={styles.checkLabel}>
+              다음에도 이 아이디를 기억합니다.
+            </label>
+            <label className={styles.forgotPassword}>
+              아이디/비밀번호를 잊으셨나요?
+            </label>
+          </div>
+          <div className={styles.signIn}> Sign in!</div>
           <div className={styles.signUpBox}>
-            <div className={styles.signUpText}></div>
-            <div className={styles.signUp}></div>
+            <label className={styles.signUpText}>
+              등록된 계정이 없으신가요?
+            </label>
+            <label className={styles.signUp}>Sign up</label>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 

53ba84408a6f7c20f0e0726d080f8fe95d0bc5fe
silver0108 <kimsoeun0108@gmail.com>
2024-07-21 00:33:21 +0900

    style(#4): 메인 이미지 삽입

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Header from './layout/Header/Header'
 
 export default function Home() {
@@ -6,10 +7,29 @@ export default function Home() {
       <section
         style={{
           height: '550px',
-          background: 'linear-gradient(60deg, #fffeff 20%, #e2eff4)'
+          background: 'linear-gradient(60deg, #fffeff 20%, #e2eff4)',
+          display: 'flex',
+          flexDirection: 'column'
         }}>
         <Header />
-        <div></div>
+        <div
+          style={{
+            flex: 1,
+            position: 'relative'
+          }}>
+          <Image
+            src="/images/home-image.png"
+            alt="Home Image"
+            width={822}
+            height={436}
+            style={{
+              position: 'absolute',
+              right: 0,
+              bottom: 0,
+              marginRight: '150px'
+            }}
+          />
+        </div>
       </section>
     </div>
   )

ee230f36d2a415bcba843c98664026d742668e18
silver0108 <kimsoeun0108@gmail.com>
2024-07-21 01:20:14 +0900

    style(#4): 메인 텍스트 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,19 @@ export default function Home() {
       <section className={styles.firstSection}>
         <Header />
         <div className={styles.introduceWrapper}>
+          <div className={styles.textWrapper}>
+            <div>
+              깔끔하고 감각적인 <br />{' '}
+              <span className={styles.highlight}>앱 UX/UI 포트폴리오</span>
+            </div>
+            <div>모아올리오에 한번에 모아 올렸습니다.</div>
+          </div>
           <div className={styles.imageWrapper}>
             <Image
               src="/images/home-image.png"
               alt="Home Image"
-              width={822}
-              height={436}
+              width={822.48}
+              height={420.11}
             />
           </div>
         </div>

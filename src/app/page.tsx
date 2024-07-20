ffad1115f9a165b389a2ab84d92065a4d117fb21
silver0108 <kimsoeun0108@gmail.com>
2024-07-21 00:53:18 +0900

    style(#4): 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,21 @@
 import Image from 'next/image'
 import Header from './layout/Header/Header'
+import styles from './Home.module.scss'
 
 export default function Home() {
   return (
-    <div>
-      <section
-        style={{
-          height: '550px',
-          background: 'linear-gradient(60deg, #fffeff 20%, #e2eff4)',
-          display: 'flex',
-          flexDirection: 'column'
-        }}>
+    <div className={styles.mainWrapper}>
+      <section className={styles.firstSection}>
         <Header />
-        <div
-          style={{
-            flex: 1,
-            position: 'relative'
-          }}>
-          <Image
-            src="/images/home-image.png"
-            alt="Home Image"
-            width={822}
-            height={436}
-            style={{
-              position: 'absolute',
-              right: 0,
-              bottom: 0,
-              marginRight: '150px'
-            }}
-          />
+        <div className={styles.introduceWrapper}>
+          <div className={styles.imageWrapper}>
+            <Image
+              src="/images/home-image.png"
+              alt="Home Image"
+              width={822}
+              height={436}
+            />
+          </div>
         </div>
       </section>
     </div>

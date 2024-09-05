2d0c8d3fb4868b4f0268ffdaa614c609e5b182b1
silver0108 <kimsoeun0108@gmail.com>
2024-09-05 23:19:54 +0900

    style(#4): 캐러셀 수정

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -43,6 +43,7 @@ const Carousel = ({ images }: CarouselProps) => {
                 src={image.link}
                 alt={image.name}
                 fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               />
             </div>
           ))}

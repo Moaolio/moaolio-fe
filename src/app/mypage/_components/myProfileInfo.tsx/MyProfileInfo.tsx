02df21b5e4278fe50dffd9b7284fba84a90f7eb3
minpppal <minpal203@gmail.com>
2024-09-16 03:00:03 +0800

    feat/map 사용하여 여러가지 선택가능 구현, 빈 값 구현/#3

--- src/app/mypage/_components/myProfileInfo.tsx/MyProfileInfo.tsx
+++ src/app/mypage/_components/myProfileInfo.tsx/MyProfileInfo.tsx
@@ -37,30 +37,58 @@ const MyProfileInfo = () => {
       <div className={styles.profileInfoContainer}>
         <div className={styles.profileUser}>
           <span className={styles.positions}>
-            프론트엔드{profileData?.positions}
+            {profileData?.positions || '포지션 없음'}
           </span>
-          <span className={styles.nickname}>민수{profileData?.nickname}</span>
+          <span className={styles.nickname}>
+            {profileData?.nickname || '닉네임 없음'}
+          </span>
           <span className={styles.introduction}>
-            안녕하세요~ 프엔 취준생 김민수입니다~{profileData?.introduction}
+            {profileData?.introduction || '자기소개 없음'}
           </span>
         </div>
         <div className={styles.userInfoBox}>
           <div className={styles.stackBox}>
             <span className={styles.stackTitle}>나의 스택목록</span>
-            <span className={styles.stack}>{profileData?.stack}</span>
+            <ul>
+              {profileData?.stack.length ? (
+                profileData.stack.map((stack, index) => (
+                  <li
+                    key={index}
+                    className={styles.stack}>
+                    {stack}
+                  </li>
+                ))
+              ) : (
+                <li className={styles.stack}>스택 없음</li>
+              )}
+            </ul>
           </div>
           <div className={styles.ExperienceBox}>
             <span className={styles.experienceTitle}>경력</span>
-            <span className={styles.experience}>
-              3년차{profileData?.experience}
-            </span>
+            <div className={styles.experience}>
+              {profileData?.experience || '경력 정보 없음'}
+            </div>
             <div className={styles.contactBox}>
               <span className={styles.contactInformationTitle}>
                 컨택 가능 주소
               </span>
-              <span className={styles.contactInformation}>
-                010-0000-0000{profileData?.contactInformation}
-              </span>
+              <ul>
+                {profileData?.contactInformation.length ? (
+                  profileData.contactInformation.map(
+                    (contactInformation, index) => (
+                      <li
+                        key={index}
+                        className={styles.contactInformation}>
+                        {contactInformation}
+                      </li>
+                    )
+                  )
+                ) : (
+                  <li className={styles.contactInformation}>
+                    연락처 정보 없음
+                  </li>
+                )}
+              </ul>
             </div>
           </div>
         </div>

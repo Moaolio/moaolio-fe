d18816649063eb0c6a81bb1966cb3affeee3df5a
minpppal <minpal203@gmail.com>
2024-10-06 15:39:29 +0800

    feat/마이페이지 데이터 전역상태관리 설정/#3

--- src/store/useMypageUpdateStore.ts
+++ src/store/useMypageUpdateStore.ts
@@ -3,5 +3,22 @@ import { ProfileState } from '@/types/mypageUpdateTypes'
 
 export const useMypagUpdateStore = create<ProfileState>(set => ({
   editProfile: false,
-  clickEditProfile: () => set(state => ({ editProfile: !state.editProfile }))
+  positions: '',
+  nickname: '',
+  introduction: '',
+  stack: [],
+  experience: '',
+  contactInformation: [],
+
+  clickEditProfile: () => set(state => ({ editProfile: !state.editProfile })),
+
+  setProfileData: data =>
+    set(() => ({
+      positions: data.positions,
+      nickname: data.nickname,
+      introduction: data.introduction,
+      stack: data.stack,
+      experience: data.experience,
+      contactInformation: data.contactInformation
+    }))
 }))
--- src/types/mypageUpdateTypes.ts
+++ src/types/mypageUpdateTypes.ts
@@ -1,4 +1,18 @@
 export interface ProfileState {
   editProfile: boolean
   clickEditProfile: () => void
+  positions: string
+  nickname: string
+  introduction: string
+  stack: string[]
+  experience: string
+  contactInformation: string[]
+  setProfileData: (data: {
+    positions: string
+    nickname: string
+    introduction: string
+    stack: string[]
+    experience: string
+    contactInformation: string[]
+  }) => void
 }

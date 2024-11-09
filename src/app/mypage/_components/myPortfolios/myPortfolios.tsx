a217eedbba7a8f8459476401838ccd94ce425674
minpppal <minpal203@gmail.com>
2024-11-10 03:46:00 +0900

    feat/나의 포트폴리오 불러오기 구현/#3

--- src/app/mypage/_components/myPortfolios/myPortfolios.tsx
+++ src/app/mypage/_components/myPortfolios/myPortfolios.tsx
@@ -1,22 +1,43 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '@/app/mypage/_components/myPortfolios/myPortfolios.module.scss'
 import Portfolio from '@/app/components/portfolio/Portfolio'
+import axios from 'axios'
+interface PortfolioData {
+  id: number
+  position: string
+  title: string
+  name: string
+}
 
 const MyPortfolios = () => {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL
+  const [portfolioData, setPortfolioData] = useState<PortfolioData[]>([])
+
+  const fetchPortfolios = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/api/post/get/portfolio`)
+      setPortfolioData(response.data)
+    } catch (error) {
+      console.error('Error fetching portfolio data:', error)
+    }
+  }
+  useEffect(() => {
+    fetchPortfolios()
+  }, [])
 
   // 예시 데이터 배열
-  const portfolioData = [
-    {
-      id: 1,
-      position: 'Frontend Developer',
-      title: 'Portfolio 1',
-      name: 'aaa'
-    },
-    { id: 2, position: 'Backend Developer', title: 'Portfolio 2', name: 'bbb' },
-    { id: 3, position: 'UI/UX Designer', title: 'Portfolio 3', name: 'ccc' }
-  ]
+  // const portfolioData = [
+  //   {
+  //     id: 1,
+  //     position: 'Frontend',
+  //     title: 'Portfolio 1',
+  //     name: 'aaa'
+  //   },
+  //   { id: 2, position: 'Backend', title: 'Portfolio 2', name: 'bbb' },
+  //   { id: 3, position: 'Designer', title: 'Portfolio 3', name: 'ccc' },
+  //   { id: 4, position: 'DS', title: 'Portfolio 4', name: 'ddd' }
+  // ]
   return (
     <>
       <div className={styles.profileInfoContainer}>
@@ -26,7 +47,7 @@ const MyPortfolios = () => {
             총 {portfolioData.length}건의 포트폴리오
           </span>
         </div>
-        <div>
+        <div className={styles.portfolioBox}>
           {portfolioData.map(portfolio => (
             <Portfolio
               key={portfolio.id}

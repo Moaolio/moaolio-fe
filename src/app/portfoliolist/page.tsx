453486de1b53a10045dee32f510dccf976d1ccae
minpppal <minpal203@gmail.com>
2024-11-14 04:14:05 +0900

    style/포트폴리오 리스트 페이지 css 구현/#34

--- src/app/portfoliolist/page.tsx
+++ src/app/portfoliolist/page.tsx
@@ -2,10 +2,11 @@
 import React from 'react'
 import styles from '@/app/portfoliolist/page.module.scss'
 import Portfolio from '../components/portfolio/Portfolio'
-
+import Header from '@/components/Header/Header'
 const Page = () => {
   return (
     <>
+      <Header />
       <div className={styles.mainContainer}>
         <div className={styles.spanBox}>
           <span className={styles.titleSpan}>인기 포트폴리오</span>
@@ -15,10 +16,10 @@ const Page = () => {
         </div>
         <div className={styles.buttonBox}>
           <div className={styles.filterBox}>
-            <button className={styles.latestButton}></button>
-            <button className={styles.popularButton}></button>
+            <button className={styles.latestButton}>최신순</button>
+            <button className={styles.popularButton}>인기순</button>
           </div>
-          <button className={styles.writeButton}></button>
+          <button className={styles.writeButton}>작성하기</button>
         </div>
         <div className={styles.portfolioList}>
           <Portfolio />
